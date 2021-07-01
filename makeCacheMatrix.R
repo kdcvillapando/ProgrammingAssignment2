f4545833ae28ab44d0a9c7b2274f54de61cce078
kdcvillapando <86752930+kdcvillapando@users.noreply.github.com>
2021-07-01 09:53:43 +0800

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -3,31 +3,31 @@
 ## makeCacheMatrix function enables us to create a matrix which could cache its corresponding inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL 
-  set <- function(y) {
+  inv.vill <- NULL 
+  set.vill <- function(y) {
                 x <<- y
-                inv <<- NULL
+                inv.vill <<- NULL
   }
-  get <- function()x
-  setinv.vill <- function(inverse)inv<<-inverse
+  get.vill <- function()x
+  setinv.vill <- function(inverse)inv.vill<<-inverse
   getinv.vill <- function(){
                 inver <- ginv(x)
                 inver%%x
   }
-  list(set = set, get = get, setinv.vill = setinv.vill, getinv.vill = getinv.vill)
+  list(set.vill = set.vill, get.vill = get.vill, setinv.vill = setinv.vill, getinv.vill = getinv.vill)
 }
 
-## On the other hand, cacheSolve function enables us to compute for the inverse of the matrix returned previously
+## On the other hand, cacheSolve function enables us to compute for the inverse of the previously returned matrix
 
 cacheSolve <- function(x, ...) {
-  inv <- x$getinv.vill()
-  if(!is.null(inv)){
+  inv.vill <- x$getinv.vill()
+  if(!is.null(inv.vill)){
                 message("now getting cached data")
-                return(inv)
+                return(inv.vill)
   }
-  data <- x$get()
-  inv <- solve(data,...)
-  x$setinv.vill(inv)
-  inv 
+  data.vill <- x$get.vill()
+  inv.vill <- solve(data.vill,...)
+  x$setinv.vill(inv.vill)
+  inv.vill
         ## Return a matrix that is the inverse of 'x'
 }
